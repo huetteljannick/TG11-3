50aa8254e0a65ebc8df02db95cc975d1757dd2ae
huetteljannick <j.huettel@hgs-singen.online>
2025-01-21 13:47:44 +0000

    Aufgabe Taschenrechner

---
+++ Java/a7Methoden/Taschenrechner.java
@@ -0,0 +1,43 @@
+package a7Methoden;
+
+import java.util.Scanner;
+
+public class Taschenrechner {
+
+    public static void main(String[] args) {
+        // Eingabe
+        Scanner s = new Scanner(System.in);
+        System.out.print("Bitte geben sie die erste Zahl ein:\t");
+        double zahl1 = s.nextDouble();
+        System.out.print("Bitte geben sie die zweite Zahl ein:\t");
+        double zahl2 = s.nextDouble();
+        s.close();
+
+        // Ausgabe
+        System.out.printf("%f + %f = %f\n", zahl1, zahl2, addiere(zahl1, zahl2));
+        System.out.printf("%f - %f = %f\n", zahl1, zahl2, subtrahiere(zahl1, zahl2));
+        System.out.printf("%f * %f = %f\n", zahl1, zahl2, multipliziere(zahl1, zahl2));
+        System.out.printf("%f / %f = %f\n", zahl1, zahl2, dividiere(zahl1, zahl2));
+      }
+   //Verarbeitung
+    public static double addiere(double zahl1, double zahl2) {
+       double ergebnis = zahl1 + zahl2;
+       return ergebnis;
+    }
+
+    public static double subtrahiere(double zahl1, double zahl2) {
+       double ergebnis = zahl1 - zahl2;
+       return ergebnis;
+    }
+
+    public static double multipliziere(double zahl1, double zahl2) {
+       double ergebnis = zahl1 * zahl2;
+       return ergebnis;
+    }
+
+    public static double dividiere(double zahl1, double zahl2) {
+       double ergebnis = zahl1 / zahl2;
+       return ergebnis;
+    }
+
+}
