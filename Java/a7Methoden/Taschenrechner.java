73545e5a35527d1d40f7499e6d06f8fa6f35f18f
huetteljannick <j.huettel@hgs-singen.online>
2025-01-21 13:57:08 +0000

    Cleanup Aufgabe Taschenrechner

--- Java/a7Methoden/Taschenrechner.java
+++ Java/a7Methoden/Taschenrechner.java
@@ -13,11 +13,10 @@ public class Taschenrechner {
         double zahl2 = s.nextDouble();
         s.close();
 
-        // Ausgabe
-        System.out.printf("%f + %f = %f\n", zahl1, zahl2, addiere(zahl1, zahl2));
-        System.out.printf("%f - %f = %f\n", zahl1, zahl2, subtrahiere(zahl1, zahl2));
-        System.out.printf("%f * %f = %f\n", zahl1, zahl2, multipliziere(zahl1, zahl2));
-        System.out.printf("%f / %f = %f\n", zahl1, zahl2, dividiere(zahl1, zahl2));
+        Ausgabe(zahl1, zahl2, '+', addiere(zahl1, zahl2));
+        Ausgabe(zahl1, zahl2, '-', subtrahiere(zahl1, zahl2));
+        Ausgabe(zahl1, zahl2, '*', multipliziere(zahl1, zahl2));
+        Ausgabe(zahl1, zahl2, '/', dividiere(zahl1, zahl2));
       }
    //Verarbeitung
     public static double addiere(double zahl1, double zahl2) {
@@ -38,6 +37,10 @@ public class Taschenrechner {
     public static double dividiere(double zahl1, double zahl2) {
        double ergebnis = zahl1 / zahl2;
        return ergebnis;
+    }
+
+    public static void Ausgabe(double zahl1, double zahl2, char Rechenzeichen, double Ergebnis){
+      System.out.printf("%f %s %f = %f\n", zahl1,Rechenzeichen, zahl2, Ergebnis);
     }
 
 }
