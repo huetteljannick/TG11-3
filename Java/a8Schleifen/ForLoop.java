4dea3226135dc09a6d7b0d83ed651f65a6f333db
huetteljannick <j.huettel@hgs-singen.online>
2025-02-03 08:58:36 +0000

    For Loop Beispiel

---
+++ Java/a8Schleifen/ForLoop.java
@@ -0,0 +1,10 @@
+package a8Schleifen;
+
+public class ForLoop {
+
+    public static void main(String[] args){
+        for (int zaehler = 0; zaehler < 5; zaehler++) {
+            System.out.println(zaehler);
+        }
+    }
+}
